645d7f0807e4f74bf90e700ca13361cba0a18139
Md Robin <jsdev.robin@gmail.com>
2026-08-11 08:41:43 +0600

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        let k: number = 0;
+
+        for(let i: number  = 0; i < nums.length; i++) {
+            if(nums[i] !== val) {
+                nums[k++] = nums[i]
+            }
+        }
+
+        return k;
+    }
+}
