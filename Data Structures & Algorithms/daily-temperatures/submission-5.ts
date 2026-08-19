cb918aaf4cd1c3b3338a7d867e2ae793c13d263b
Md Robin <jsdev.robin@gmail.com>
2026-08-19 23:17:02 +0600

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        const res: number[] = new Array(temperatures.length).fill(0);
+        const stack: number[] = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+                const idx = stack.pop()!;
+                res[idx] = i - idx;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
