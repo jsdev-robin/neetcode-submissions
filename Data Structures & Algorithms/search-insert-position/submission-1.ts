28ff06b21592d45521ae993a1d70a122873c4f28
Md Robin <jsdev.robin@gmail.com>
2026-08-21 22:13:22 +0600

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums: number[], target: number): number {
+        let l: number = 0;
+        let r: number = nums.length - 1;
+
+        while (l <= r) {
+            const mid = Math.floor((l + r) / 2);
+            if (nums[mid] === target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return l;
+    }
+}
