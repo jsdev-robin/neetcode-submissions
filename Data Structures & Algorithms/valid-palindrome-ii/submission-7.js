1739067a8bdcef24f3fcac4f1ceb30bda6eb99f9
Md Robin <jsdev.robin@gmail.com>
2026-09-01 15:30:48 +0600

    Add: valid-palindrome-ii - submission-7

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-7.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let isPalin = (l, r) => {
+            while (l < r) {
+                if (s[l] !== s[r]) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+
+            return true;
+        };
+
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            if (s[l] !== s[r]) {
+                return isPalin(l + 1, r) || isPalin(l, r - 1);
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
