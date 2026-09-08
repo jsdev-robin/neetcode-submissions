f1e49a7bc3c3debb9cd2c1dee0a168db2848b31e
Md Robin <jsdev.robin@gmail.com>
2026-09-09 00:57:48 +0600

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+        if (strs.length === 0) {
+            return "";
+        }
+
+        let prefix: string = strs[0];
+
+        for (let i: number = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) !== 0) {
+                prefix = prefix.slice(0, -1);
+                if (prefix === "") {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
